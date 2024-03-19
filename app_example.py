5bd2cef48dbcbac6062aaf4180791db98fc0721d
Gorbatova <abgorbatova@rt-dc.ru>
2024-03-19 12:59:16 +0300

    db class tarantool connect

--- app_example.py
+++ app_example.py
@@ -1,3 +1,6 @@
+'''
+app which connects to tarantool and inserts data
+'''
 import asyncio
 import asynctnt
 import time
@@ -9,9 +12,9 @@ PORT1 = 3303
 PORT2 = 3304
 
 '''for tarantool in docker version 3
-sudo docker exec -it t3 console
+sudo docker exec -it t3 console     # t3 - container_name (replace with your container_name)
 sudo docker exec -it t4 console
-Команды в консоли тарантула (запущенного в докере)
+Команды в консоли тарантула (запущенного в докере)  # following are commands for tarantool in docker
 
 box.cfg {
         memtx_memory = 1 * 1000 * 1024 * 1024,
@@ -35,42 +38,95 @@ box.space.APP:create_index("primary", {parts={{field = 1, type = 'uuid'}}})
 box.space.APP:create_index('name', { unique = false, parts = { 'BAND_NAME' } })
 box.space.APP:create_index('time', { unique = false, parts = { 'YEAR' } })
 '''
+class Db:
+    conn = None
 
-async def get_conn(port):
-    conn = asynctnt.Connection(host="localhost", port=port, username='admin', password='pass')
-    await conn.connect()
-    return conn
+    async def get_conn(self):
+        if self.conn:
+            return self.conn
+        return await self.db_reconnect()
+
+    async def db_reconnect(self):
+        '''reconnect_timeout=None'''
+        ports = (PORT1, PORT2)
+        print("trying to connect...")
+        for port in ports:
+            try:
+                self.conn = asynctnt.Connection(host="localhost", port=port, username='admin', password='pass', reconnect_timeout=None)
+                await self.conn.connect()
+            except ConnectionRefusedError:
+                print("ConnectionRefusedError")
+            except Exception as exc:
+                print(exc)
+
+            if self.conn:
+                print(port)
+                break
+        return self.conn
+    
+    async def db_disconnect(self):
+        if self.conn:
+            await self.conn.disconnect()
+            self.conn = None
+
+    async def execute(self, sql, args=tuple()):
+        conn = await self.get_conn()
+        res = None
+        if conn:
+            try:
+                res = await conn.execute(sql, args)
+            except Exception as exc:
+                print(exc)
+                raise exc
+        return res
+
+    async def select(self, sql, args=tuple()):
+        conn = await self.get_conn()
+        res = None
+        if conn:
+            if not args:
+                data = await conn.execute(sql)
+            else:
+                data = await conn.execute(sql, args)
+            return data
 
 
-async def insert(conn, val):
-    await conn.execute('insert into APP (ID, BAND_NAME, YEAR) values (?, ?, ?);', (uuid.uuid4(), "", val))
 
-
-async def print_select(conn):
-    data = await conn.execute('select * from SEQSCAN  APP order by YEAR desc limit 5;')
+async def print_select(db):
+    sql = 'select * from SEQSCAN APP order by YEAR desc limit 5;'
+    data = await db.select(sql)
     for row in data:
         print(row)
     print()
 
 
-async def main():
+
+async def process_with_db(db, start=0, stop=1000):
     try:
-        conn = await get_conn(PORT1)
-        print(PORT1)
-    except Exception:
-        conn = await get_conn(PORT2)
-        print(PORT2)
+        for i in range(start, stop):
+            time_val = int(datetime.now().timestamp())
+           
+            sql = 'insert into APP (ID, BAND_NAME, YEAR) values (?, ?, ?);'
+            args = (uuid.uuid4(), "", time_val)
+            await db.execute(sql, args)
+
+            if i % 10 == 1:
+                await print_select(db)
+            time.sleep(2)
+            i += 1
+    except Exception as exc:
+        print(f"{i}--------Not finished {exc}")
+    return i
     
 
-    for i in range(1000):
-        time_val = int(datetime.now().timestamp())
-        await insert(conn, time_val)
 
-        if i % 10 == 1:
-            await print_select(conn)
-        time.sleep(2)
-
-    await conn.disconnect()
+async def main():
+    db = Db()
+    stop = 30
+    i = await process_with_db(db, 0, stop)
+    await db.db_disconnect()
+    if i == stop:
+        print('finished successfully')
 
 
 
